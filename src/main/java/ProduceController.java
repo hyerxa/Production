1bd8a6b2c89bd9233134591069d956ab89d140c0
Haley Yerxa <hbyerxa7681@eagle.fgcu.edu>
2020-09-02 19:07:11 -0400

    add fxml items for lesson 3

--- src/main/java/ProduceController.java
+++ src/main/java/ProduceController.java
@@ -1,2 +1,14 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ProduceController {
+
+    @FXML
+    Button recordProduction;
+
+    @FXML
+    private void setRecordProduction(ActionEvent event) {
+        System.out.println("Record Production");
+    }
 }
--- src/main/java/ProductLineController.java
+++ src/main/java/ProductLineController.java
@@ -1,2 +1,14 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ProductLineController {
+
+    @FXML
+    Button addProduct;
+
+    @FXML
+    private void setAddProduct(ActionEvent event) {
+        System.out.println("Add Product");
+    }
 }
