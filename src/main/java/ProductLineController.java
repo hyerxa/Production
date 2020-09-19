9d1e9f820ad49a1bd169d58fd751062ec27a8898
Haley Yerxa <hbyerxa7681@eagle.fgcu.edu>
2020-09-19 13:46:14 -0400

    add database and tables

--- src/main/java/ProductLineController.java
+++ src/main/java/ProductLineController.java
@@ -2,6 +2,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
+import java.sql.*;
+
 public class ProductLineController {
 
     @FXML
@@ -11,4 +13,47 @@ public class ProductLineController {
     private void setAddProduct(ActionEvent event) {
         System.out.println("Add Product");
     }
+
+    public void initialize() {
+        connectToDb();
+    }
+
+    public void connectToDb() {
+        final String JDBC_DRIVER = "org.h2.Driver";
+        final String DB_URL = "jdbc:h2:./res/HR";
+
+        //  Database credentials
+        final String USER = "";
+        final String PASS = "";
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            // STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 2: Open a connection
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 3: Execute a query
+            stmt = conn.createStatement();
+
+            String sql = "SELECT * FROM place ";
+
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+            // STEP 4: Clean-up environment
+            stmt.close();
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
