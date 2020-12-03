991f03b18affb7360dbc30c3f3b752cb6da9b861
Haley Yerxa <haley.yerxa24@gmail.com>
2020-12-03 18:29:39 -0500

    add jar file

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,13 @@
+/**
+ * Launches Main Class. Used for Deployment
+ *
+ * @author Haley Yerxa
+ */
+
+public class Launcher {
+
+  public static void main(String[] args) {
+    Main.main(args);
+  }
+
+}
